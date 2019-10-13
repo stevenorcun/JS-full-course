14e0e8ea89dc6e0c4eeb3955cba7465f22e67db5
StevOrc <orcun.steven@yahoo.fr>
2019-10-13 21:46:13 +0200

    adding playground.js

---
+++ chapter1/playground.js
@@ -0,0 +1,18 @@
+
+var steven = "Steven";
+var country;
+
+var fullAge;
+
+switch(country){
+    case "France":
+        fullAge = 18; break;
+    case "USA":
+        fullAge = 21; break;
+    case "Sweden":
+        fullAge = 19;break;
+    default:
+        fullAge = 21; break;
+}
+
+console.log(`full age is equal to : ${fullAge}`);
